5f16f86bc318a23fb89f86f87d2c63d21b883a6d
morenoa <Amoreno3657@Adams-MacBook-Pro.local>
2017-03-05 20:24:37 -0600

    Initial commit

---
+++ app/src/main/java/net/androidbootcamp/mobilefoodmenu/MainActivity.java
@@ -0,0 +1,64 @@
+package net.androidbootcamp.mobilefoodmenu;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Integer[] food = {R.drawable.pizza_burger, R.drawable.veggie_burger, R.drawable.burger};
+    ImageView pic;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        GridView grid = (GridView)findViewById(R.id.gridView);
+        final  ImageView pic = (ImageView)findViewById(R.id.imgLarge);
+        grid.setAdapter(new ImageAdapter(this));
+        grid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent i = new Intent(MainActivity.this, Pizza_Burger.class);
+                startActivity(i);
+            }
+        });
+    }
+
+    public class ImageAdapter extends BaseAdapter {
+        private Context context;
+        public ImageAdapter (Context c){
+            context = c;
+        }
+        @Override
+        public int getCount() {
+            return food.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            pic = new ImageView(context);
+            pic.setImageResource(food[position]);
+            pic.setScaleType(ImageView.ScaleType.FIT_XY);
+            pic.setLayoutParams(new GridView.LayoutParams(330,300));
+            return pic;
+        }
+    }
+}
---
+++ app/src/main/java/net/androidbootcamp/mobilefoodmenu/Meaty_Burger.java
@@ -0,0 +1,13 @@
+package net.androidbootcamp.mobilefoodmenu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Meaty_Burger extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meaty__burger);
+    }
+}
